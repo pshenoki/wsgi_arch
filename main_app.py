c8873ccde6e740b1808a1562a7b409edd2a02521
pshenoki <suslik1991@yandex.ru>
2022-02-04 17:34:52 +0300

    less_5_1

--- main_app.py
+++ main_app.py
@@ -4,3 +4,5 @@ from urls import ROUTES, FRONTS
 run_object = Application(routes=ROUTES, fronts=FRONTS)
 # run_object = ProxyLogApplication(routes=ROUTES, fronts=FRONTS)
 # run_object = ProxyFakeApplication(routes=ROUTES, fronts=FRONTS)
+
+
